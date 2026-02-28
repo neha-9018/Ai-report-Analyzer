2af4bd2a907fafe400b589ee3c912492eb329c3d
Neha Yadav <nehay9018@gmail.com>
2026-02-28 22:27:11 +0530

    Add Navbar and ResumeCard components

---
+++ app/Components/ResumeCard.tsx
@@ -0,0 +1,7 @@
+import type { JSX } from "react";
+
+const ResumeCard: () => JSX.Element = () => {
+    return <div>ResumeCard</div>;
+};
+
+export default ResumeCard;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,29 @@
+
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/Components/Navbar";
+import {resumes} from "../../constant";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')]bg-cover">
+    <Navbar />
+<section className="main-section">
+  <div className="Page-heading">
+    <h1> Track your Application and Resume Rating</h1>
+    <h2>Review your Submissions and Check AI-powered Feedback</h2>
+  </div>
+</section>
+    {resumes.map((resume) => (
+        <div key={resume.id}>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+    ))}
+
+    </main>
 }
